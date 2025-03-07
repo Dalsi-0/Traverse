ccc6916625c150bb4b0af34b000aa1eef5dc7b32
dalsi <tkdwns8632@gmail.com>
2025-03-07 15:33:34 +0900

    feat: 점프패드 오브젝트의 현재 방향에 따라 튕기도록 수정

--- Assets/02_Scripts/Etc/JumpPad.cs
+++ Assets/02_Scripts/Etc/JumpPad.cs
@@ -3,30 +3,25 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     public float bounceForce = 10f; // 튕기는 힘
-    public float height = 5f; // 튕겨내는 높이
+
+    PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = PlayerManager.Instance.GetPlayerReferences().PlayerController;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         // 플레이어가 점프 중이거나 낙하 중일 때만 튕겨내기
         if (collision.gameObject.CompareTag("Player"))
         {
-            PlayerController playerController = PlayerManager.Instance.GetPlayerReferences().PlayerController;
+            playerController.PlayerRigidbody.velocity = Vector3.zero;
 
-            // 플레이어가 점프 중이거나 낙하 중일 때만 처리
-            if (playerController != null && playerController.PlayerRigidbody.velocity.y < 0)
-            {
-                // 점프대 위에 있는 모든 오브젝트 튕겨내기
-                Collider[] objectsAbove = Physics.OverlapBox(transform.position, new Vector3(5f, 10f, 5f), Quaternion.identity);
-                foreach (Collider obj in objectsAbove)
-                {
-                    Rigidbody rb = obj.gameObject.GetComponent<Rigidbody>();
-                    if (rb != null)
-                    {
-                        // 위로 튕겨내는 힘을 추가
-                        rb.AddForce(Vector3.up * height + Vector3.up * bounceForce, ForceMode.Impulse);
-                    }
-                }
-            }
+            // 점프대의 현재 "위쪽 방향(transform.up)"으로 힘을 줌
+            Vector3 bounceDirection = transform.up;
+
+            playerController.PlayerRigidbody.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
         }
     }
 }
